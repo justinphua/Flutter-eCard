4358a0e9d2967e9a22a4dfa76d11abcde0667854
justinphua <justinphua85@gmail.com>
2021-08-08 21:45:35 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Name Card'),
+        ),
+        body: SafeArea(
+            child: Column(
+          children: [
+            Text('Your Name'),
+            Text('Your Company'),
+          ],
+        )),
+      ),
+    );
+  }
+}
